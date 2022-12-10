93ef43ca9d3430bb9616b4789e5b9ff47a4aca91
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-12-10 20:03:41 +0500

    Updated: 1. Colors 2. Set NoActionBar for the Themes

--- app/src/main/java/org/abubaker/distancetracker/MainActivity.kt
+++ app/src/main/java/org/abubaker/distancetracker/MainActivity.kt
@@ -2,10 +2,32 @@ package org.abubaker.distancetracker
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import org.abubaker.distancetracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Inflate Layout: @layout/activity_main.xml
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //
+        binding.lifecycleOwner = this
+
+        // Find the Navigation Controller
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+
     }
+
 }
