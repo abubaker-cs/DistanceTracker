5bd48f4950ae960872089a86c41c41ca95b63d7a
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-12-13 00:53:34 +0500

    Reviewing code

--- app/src/main/java/org/abubaker/distancetracker/MapsFragment.kt
+++ app/src/main/java/org/abubaker/distancetracker/MapsFragment.kt
@@ -7,16 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.CameraPosition
 import com.vmadalin.easypermissions.EasyPermissions
 import com.vmadalin.easypermissions.dialogs.SettingsDialog
 import kotlinx.coroutines.delay
@@ -27,6 +24,7 @@ import org.abubaker.distancetracker.util.ExtensionFunctions.hide
 import org.abubaker.distancetracker.util.ExtensionFunctions.show
 import org.abubaker.distancetracker.util.Permissions.hasBackgroundLocationPermission
 import org.abubaker.distancetracker.util.Permissions.requestBackgroundLocationPermission
+
 
 class MapsFragment : Fragment(),
     OnMapReadyCallback,
@@ -40,17 +38,11 @@ class MapsFragment : Fragment(),
     private val binding get() = _binding!!
 
     private lateinit var map: GoogleMap
+    private lateinit var cameraPosition: CameraPosition
 
-    val started = MutableLiveData(false)
-
-    private var startTime = 0L
-    private var stopTime = 0L
-
-    private var locationList = mutableListOf<LatLng>()
-    private var polylineList = mutableListOf<Polyline>()
-    private var markerList = mutableListOf<Marker>()
-
+    // The entry point to the Fused Location Provider.
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -63,6 +55,10 @@ class MapsFragment : Fragment(),
 
         // Specify the current activity as the lifecycle owner
         binding.lifecycleOwner = viewLifecycleOwner
+
+        // Construct a FusedLocationProviderClient.
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
 
         binding.apply {
 
@@ -83,25 +79,26 @@ class MapsFragment : Fragment(),
 
         }
 
-        // Get the SupportMapFragment and request notification when the map is ready to be used.
-        fusedLocationProviderClient =
-            LocationServices.getFusedLocationProviderClient(requireActivity())
-
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Build the map.
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+
         mapFragment?.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
 
+        // Assigning the map
         map = googleMap
 
-        // We also need to call our google map object to enable the current location button
+        // This will display the MY LOCATION BUTTON on the map
         map.isMyLocationEnabled = true
 
         // important to enable this feature
@@ -110,11 +107,36 @@ class MapsFragment : Fragment(),
         map.uiSettings.apply {
 
             // Disabling these functionalities
+
+            // Static Image View
+            // ==============================================
+            // 1. Fixed Position: Disables camera movement
+            // 2. Disabled Zoom functionality
+            // isScrollGesturesEnabled = false
+            // isZoomGesturesEnabled = false
             isZoomControlsEnabled = false
+
+            // My Location
+            // * Requires enabling my-location layer
+            // ==============================================
+            isMyLocationButtonEnabled = true
+
+            // Enables: Zoom +/- buttons on the map
             isZoomGesturesEnabled = false
+
             isRotateGesturesEnabled = false
+
             isTiltGesturesEnabled = false
+
+            // on Rotation
             isCompassEnabled = false
+
+            // onPress Toolbar
+            // ==============================================
+            // 1. Go to Google Maps
+            // 2. Find Direction
+            isMapToolbarEnabled = false
+
             isScrollGesturesEnabled = false
 
         }
@@ -127,6 +149,9 @@ class MapsFragment : Fragment(),
 
         // We are checking if we have background location permission
         if (hasBackgroundLocationPermission(requireContext())) {
+
+            // This will display the MY LOCATION BUTTON on the map
+            map.isMyLocationEnabled = true
 
             Log.d("MapsFragment", "Already Enabled")
 
@@ -157,6 +182,13 @@ class MapsFragment : Fragment(),
     // This method is called when the user clicks the My Location button on the map.
     override fun onMyLocationButtonClick(): Boolean {
 
+        //  map.isMyLocationEnabled = true
+        // map.uiSettings.isMyLocationButtonEnabled = true
+
+
+        // TODO Move the camera to the location with ZOOM Level
+        // map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(34.04692127928215, -118.24748421830992), 10f))
+
         binding.tvHint.animate().alpha(0f).duration = 1500
 
         lifecycleScope.launch {
@@ -175,7 +207,11 @@ class MapsFragment : Fragment(),
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        // super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // This will redirect the user to his current location
+        map.isMyLocationEnabled = true
+
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 
